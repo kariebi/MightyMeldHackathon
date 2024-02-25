1412141b55e0f460d4fd48fbe6def35a10455d4e
kariebi <sorohmichael@gmail.com>
2024-02-25 12:12:07 -0500

    alignment for the settings component

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -8,8 +8,8 @@ const Settings = () => {
     const [showSettings, setshowSettings] = useState(false)
     const speakerclass = "rounded-full w-[20px] h-[20px] p-[3px]"
     return (
-        <div className='fixed bottom-[64px] right-3'>
-            <div onClick={() => { setshowSettings(!showSettings) }} className={`fixed bottom-2 right-2 transition duration-500 shadow-md ${showSettings ? "bg-blue-500" : "bg-blue-200"} rounded-full p-2 z-10`}>
+        <div className='fixed bottom-[72px] right-6'>
+            <div onClick={() => { setshowSettings(!showSettings) }} className={`fixed bottom-4 right-4 transition duration-500 shadow-md ${showSettings ? "bg-blue-500" : "bg-blue-200"} rounded-full p-2 z-10`}>
                 <IoSettings className={`transition text-[30px] duration-200 ${showSettings ? "rotate-180 text-blue-200" : "text-blue-600 rotate-0"}`} />
             </div>
             <div className={`text-blue-600 bg-white/60 pl-1 py-0.5 w-[82px] backdrop-blur-md rounded-md flex flex-col transition-all duration-500 gap-1 ${showSettings ? "scale-100 opacity-100 translate-y-0" : " translate-y-20 translate-x-10 opacity-0 scale-0"}`}>
